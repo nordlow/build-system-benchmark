aeee9ae794eec2a66f6194ada0fd1c40b4cb37bc
Per Nordlöw <per.nordlow@gmail.com>
2017-05-22 14:11:12 +0200

    Bump inotify

--- increate_inotify_limits.sh
+++ increate_inotify_limits.sh
@@ -1,1 +1,3 @@
-sudo sysctl fs.inotify.max_user_watches=524288
+sysctl fs.inotify.max_user_watches=524288
+sysctl fs.inotify.max_queued_events=524288 # originally 16384
+sysctl fs.inotify.max_user_instances=4096 # originally 128
