2b65fc8991c7bcc66c9e251b0f592b721487ed8f
Per Nordlöw <per.nordlow@gmail.com>
2017-05-22 14:12:14 +0200

    Add comment

--- test_bazel.sh
+++ test_bazel.sh
@@ -4,4 +4,6 @@ CPU_COUNT="$(grep -c processor /proc/cpuinfo)";
 JOB_COUNT=$((${CPU_COUNT} + 1))
 
 ./generate_sources.py
+
+./increase_inotify_limits.sh    # needed by Bazel's flag `--watchfs`
 time bazel --watchfs build -j ${JOB_COUNT} :bench
