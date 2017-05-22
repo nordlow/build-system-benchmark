7ba12e0fe05dd9dfec0e0f698e97b6d5f86af4c7
Per Nordlöw <per.nordlow@gmail.com>
2017-05-22 14:22:37 +0200

    Fix

--- increase_inotify_limits.sh
+++ increase_inotify_limits.sh
@@ -1,3 +1,3 @@
-sysctl fs.inotify.max_user_watches=524288
-sysctl fs.inotify.max_queued_events=524288 # originally 16384
+sysctl fs.inotify.max_user_watches=1000000
+sysctl fs.inotify.max_queued_events=1000000 # originally 16384
 sysctl fs.inotify.max_user_instances=4096 # originally 128
