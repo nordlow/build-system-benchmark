4caa76fd2952727cca46c48d95e9d5babe385c06
Per Nordlöw <per.nordlow@gmail.com>
2017-05-22 11:31:03 +0200

    Add source code generation

--- generate_scons_profile_dot.sh
+++ generate_scons_profile_dot.sh
@@ -3,4 +3,5 @@
 SCONS_PROFILE=$(mktemp --suffix=.profile)
 SCONS_DOT_OUTPUT=$(mktemp --suffix=.png)
 
+./generate_sources.py
 scons -s -Q --profile=${SCONS_PROFILE} && gprof2dot -f pstats ${SCONS_PROFILE} | dot -Tpng -o ${SCONS_DOT_OUTPUT} && xdg-open ${SCONS_DOT_OUTPUT}
