5c48c230bc043f47d0b27d8c9e5bcf70207f1b08
Per Nordlöw <per.nordlow@gmail.com>
2017-05-22 12:44:14 +0200

    Add watchfs flag

--- test_bazel.sh
+++ test_bazel.sh
@@ -4,4 +4,4 @@ CPU_COUNT="$(grep -c processor /proc/cpuinfo)";
 JOB_COUNT=$((${CPU_COUNT} + 1))
 
 ./generate_sources.py
-time bazel build -j ${JOB_COUNT} :bench
+time bazel --watchfs build -j ${JOB_COUNT} :bench
