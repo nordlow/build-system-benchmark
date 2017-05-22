80e1deda7c6403322744fa93aeb53751dadc25bb
Per Nordlöw <per.nordlow@gmail.com>
2017-05-22 11:28:37 +0200

    Better

--- generate_scons_profile_dot.sh
+++ generate_scons_profile_dot.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-scons -s -Q --profile=scons.profile && gprof2dot -f pstats scons.profile | dot -Tpng -o output.png && o output.png
+SCONS_PROFILE=$(mktemp --sufix=.profile)
+SCONS_DOT_OUTPUT=$(mktemp --suffix=.png)
+
+scons -s -Q --profile=${SCONS_PROFILE} && gprof2dot -f pstats ${SCONS_PROFILE} | dot -Tpng -o ${SCONS_DOT_OUTPUT} && gnome-open ${SCONS_DOT_OUTPUT}
