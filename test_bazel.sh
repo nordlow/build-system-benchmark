cdf26c0b51702ab67ec521853a9a3c3f4aa0c0b4
Per Nordlöw <per.nordlow@gmail.com>
2017-05-23 13:56:32 +0200

    Disable --watchfs flag

--- test_bazel.sh
+++ test_bazel.sh
@@ -5,5 +5,5 @@ JOB_COUNT=$((${CPU_COUNT} + 1))
 
 ./generate_sources.py
 
-sudo ./increase_inotify_limits.sh    # needed by Bazel's flag `--watchfs`
-time bazel --watchfs build -j ${JOB_COUNT} :bench
+# sudo ./increase_inotify_limits.sh    # needed by Bazel's flag `--watchfs`
+time bazel build -j ${JOB_COUNT} :bench # TODO try out --watchfs when it works
