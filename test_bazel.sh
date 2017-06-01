66083b9c675e873faaec14ec8197b52ee6e6ae58
Per Nordlöw <per.nordlow@gmail.com>
2017-06-01 14:32:13 +0200

    Add watchfs

--- test_bazel.sh
+++ test_bazel.sh
@@ -6,4 +6,4 @@ JOB_COUNT=$((${CPU_COUNT} + 1))
 ./generate_sources.py
 
 # sudo ./increase_inotify_limits.sh    # needed by Bazel's flag `--watchfs`
-time bazel build -j ${JOB_COUNT} :bench # TODO try out --watchfs when it works
+time bazel --watchfs build -j ${JOB_COUNT} :bench # TODO try out --watchfs when it works
