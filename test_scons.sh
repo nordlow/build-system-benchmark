21fd0f8a67efce0c7cb78ded1ef321a8ce9e3108
Per Nordlöw <per.nordlow@gmail.com>
2017-06-01 21:42:20 +0200

    Fixes

--- test_scons.sh
+++ test_scons.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 CPU_COUNT="$(grep -c processor /proc/cpuinfo)";
+JOB_COUNT=$((${CPU_COUNT} + 1))
 
 ./generate_sources.py
-time scons -s -j ${CPU_COUNT}
+
+time scons -s -j ${JOB_COUNT}
