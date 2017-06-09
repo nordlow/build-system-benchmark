a3ced64e476a1b56e7d04b66f56234e287e44212
Per Nordlöw <per.nordlow@gmail.com>
2017-06-09 16:27:18 +0200

    Indent

--- test_bazel.sh
+++ test_bazel.sh
@@ -6,4 +6,9 @@ JOB_COUNT=$((${CPU_COUNT} + 1))
 ./generate_sources.py
 
 # sudo ./increase_inotify_limits.sh    # needed by Bazel's flag `--watchfs`
-time bazel --watchfs build --copt="-Wall" -j ${JOB_COUNT} :bench # TODO try out --watchfs when it works
+time bazel --watchfs \
+     build \
+     --experimental_action_listener=//:cppcheck_c_cpp \
+     --copt="-Wall" \
+     -j ${JOB_COUNT} \
+     :bench # TODO try out --watchfs when it works
