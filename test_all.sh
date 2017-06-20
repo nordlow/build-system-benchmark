8e48609c3aae44b02d7e287c371e8bc1ef3103a7
Per Nordlöw <per.nordlow@gmail.com>
2017-06-20 16:50:32 +0200

    Fix

---
+++ test_all.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+CPU_COUNT="$(grep -c processor /proc/cpuinfo)";
+JOB_COUNT=$((${CPU_COUNT} + 1))
+
+./generate_sources.py
+
+# SCons
+echo "Benchmarking SCons ..."
+time scons -s -j ${JOB_COUNT}
+
+# Bazel
+echo "Benchmarking Bazel ..."
+pushd src
+time bazel \
+     build \
+     --copt="-Wall" \
+     -j ${JOB_COUNT} \
+     //... # TODO try out --watchfs when it works
+popd
