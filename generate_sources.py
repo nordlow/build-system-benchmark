8260207a256bc25fdcbc7839ed46d4b4ee728346
Per Nordlöw <per.nordlow@gmail.com>
2017-06-20 14:40:27 +0200

    Better gen structure

--- generate_sources.py
+++ generate_sources.py
@@ -4,47 +4,54 @@ import os
 import shutil
 
 
-def gen(root_path='src',
-        file_count=1000,
-        header_count=1000):
+def regenerate_sources(root_path='src',
+                       lib_count=100,
+                       file_count=100,
+                       header_count=100):
 
     shutil.rmtree(root_path, ignore_errors=True)
 
     os.makedirs(root_path, exist_ok=True)
 
-    for i in range(file_count):
-        istr = '{num:05d}'.format(num=i)
-        f = open(root_path+'/test_'+istr+'.c', 'w')
+    for lib_index in range(0, lib_count):
+        lib_prefix = 'lib' + '{num:03d}'.format(num=lib_index)
 
-        # include som C-header
-        f.write('''#include <stdio.h>
+        sub_path = os.path.join(root_path, lib_prefix)
+        os.makedirs(sub_path, exist_ok=True)
+
+        for i in range(file_count):
+            istr = '{num:05d}'.format(num=i)
+            test_prefix = 'source'+istr+'.c'
+            with open(os.path.join(sub_path, lib_prefix + '_' + test_prefix), 'w') as f:
+
+                # include som C-header
+                f.write('''#include <stdio.h>
 #include <stdlib.h>
 
 ''')
 
-        # include local headers
-        for j in range(header_count):
-            jstr = '{num:05d}'.format(num=j)
-            f.write('''#include "utils_''' + jstr + '''.h"\n''')
+                # include local headers
+                for j in range(header_count):
+                    jstr = '{num:05d}'.format(num=j)
+                    f.write('''#include "utils_''' + jstr + '''.h"\n''')
 
-            f.write('''
-int ffff_''' + jstr + '''(int x)
+                    f.write('''
+int function''' + jstr + '''(int x)
 {
     return x+1;
 }
 ''')
-        f.close()
 
-    for i in range(header_count):
-        istr = '{num:05d}'.format(num=i)
-        f = open(root_path+'/utils_'+istr+'.h', 'w')
-        f.write('''#include <stdio.h>
+        for i in range(header_count):
+            istr = '{num:05d}'.format(num=i)
+            with open(os.path.join(sub_path, 'utils_'+istr+'.h'), 'w') as f:
+                f.write('''#include <stdio.h>
 #include <stdlib.h>
 
 int utils_''' + istr + '''(int x);
 ''')
-        f.close()
+
 
 if (__name__ == '__main__'):
-    # gen(*sys.argv[1:])
-    gen()
+    # regenerate_sources(*sys.argv[1:])
+    regenerate_sources()
