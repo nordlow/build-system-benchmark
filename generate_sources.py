7eca4a92215728d093d8273689e9f5118ac16a52
Per Nordlöw <per.nordlow@gmail.com>
2017-06-20 15:15:43 +0200

    Better

--- generate_sources.py
+++ generate_sources.py
@@ -4,72 +4,74 @@ import os
 import shutil
 
 
-def regenerate_sources(root_path='src',
+def regenerate_sources(src_path='src',
                        lib_count=2000,
                        file_count=10,
                        header_count=10):
 
-    shutil.rmtree(root_path, ignore_errors=True)
-
-    os.makedirs(root_path, exist_ok=True)
-
     # generate top Bazel BUILD file
-    with open(os.path.join(root_path, "BUILD"), 'w') as f:
+    with open(os.path.join("BUILD"), 'w') as f:
         f.write('''''')
 
     # generate top Bazel WORKSPACE file
-    with open(os.path.join(root_path, "WORKSPACE"), 'w') as f:
+    with open(os.path.join("WORKSPACE"), 'w') as f:
         f.write('''''')
 
+    # empty src tree
+    shutil.rmtree(src_path, ignore_errors=True)
 
-    for lib_index in range(0, lib_count):
-        lib_name = 'lib' + '{num:03d}'.format(num=lib_index)
+    os.makedirs(src_path, exist_ok=True)
 
-        sub_path = os.path.join(root_path, lib_name)
-        os.makedirs(sub_path, exist_ok=True)
+    with open(os.path.join("SConstruct"), 'w') as sconstruct_file:
 
-        # generate source file
-        for i in range(file_count):
-            istr = '{num:05d}'.format(num=i)
-            test_file_name = 'source'+istr+'.c'
-            with open(os.path.join(sub_path, lib_name + '_' + test_file_name), 'w') as f:
+        for lib_index in range(0, lib_count):
+            lib_name = 'lib' + '{num:03d}'.format(num=lib_index)
 
-                # include som C-header
-                f.write('''#include <stdio.h>
-#include <stdlib.h>
+            sub_path = os.path.join(src_path, lib_name)
+            os.makedirs(sub_path, exist_ok=True)
 
-''')
+            # generate source file
+            for i in range(file_count):
+                istr = '{num:05d}'.format(num=i)
+                test_file_name = 'source'+istr+'.c'
+                with open(os.path.join(sub_path, lib_name + '_' + test_file_name), 'w') as f:
 
-                # include local headers
-                for j in range(header_count):
-                    jstr = '{num:05d}'.format(num=j)
-                    f.write('''#include "utils_''' + jstr + '''.h"\n''')
+                    # include som C-header
+                    f.write('''#include <stdio.h>
+    #include <stdlib.h>
 
-                    f.write('''
-int function''' + jstr + '''(int x)
-{
-    return x+1;
-}
-''')
+    ''')
 
-        # generate header file
-        for i in range(header_count):
-            istr = '{num:05d}'.format(num=i)
-            with open(os.path.join(sub_path, 'utils_'+istr+'.h'), 'w') as f:
-                f.write('''#include <stdio.h>
-#include <stdlib.h>
+                    # include local headers
+                    for j in range(header_count):
+                        jstr = '{num:05d}'.format(num=j)
+                        f.write('''#include "utils_''' + jstr + '''.h"\n''')
 
-int utils_''' + istr + '''(int x);
-''')
+                        f.write('''
+    int function''' + jstr + '''(int x)
+    {
+        return x+1;
+    }
+    ''')
 
-        # generate Bazel BUILD file
-        with open(os.path.join(sub_path, "BUILD"), 'w') as f:
-            f.write('''cc_library(
-    name = "{}",
-    srcs = glob(["*.c", "*.h"]),
-    linkstatic = 1,
-)
-'''.format(lib_name))
+            # generate header file
+            for i in range(header_count):
+                istr = '{num:05d}'.format(num=i)
+                with open(os.path.join(sub_path, 'utils_'+istr+'.h'), 'w') as f:
+                    f.write('''#include <stdio.h>
+    #include <stdlib.h>
+
+    int utils_''' + istr + '''(int x);
+    ''')
+
+            # generate Bazel BUILD file
+            with open(os.path.join(sub_path, "BUILD"), 'w') as f:
+                f.write('''cc_library(
+        name = "{}",
+        srcs = glob(["*.c", "*.h"]),
+        linkstatic = 1,
+    )
+    '''.format(lib_name))
 
 
 if (__name__ == '__main__'):
