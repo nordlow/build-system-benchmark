4508c70ea12da8ac3eda2162dcbf4362edcab990
Per Nordlöw <per.nordlow@gmail.com>
2017-06-20 17:14:47 +0200

    Add test_buck

---
+++ benchmark_buck.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+CPU_COUNT="$(grep -c processor /proc/cpuinfo)";
+JOB_COUNT=$((${CPU_COUNT} + 1))
+
+echo
+echo "######################"
+echo "Benchmarking Buck ..."
+
+time buck build -j ${JOB_COUNT} :bench
